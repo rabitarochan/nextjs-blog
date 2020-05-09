3145f9a17377020a32ef2e4752bc761d6624e887
rabitarochan <rabitarochan@gmail.com>
2020-05-09 09:20:01 +0900

    名前を変更

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm Kengo. I'm a software engeneer. You can contact me on <a href="https://twitter.com/rabitarochan" target="_blank">Twitter</a></p>
+        <p>Hello, I'm rabitarochan. I'm a software engeneer. You can contact me on <a href="https://twitter.com/rabitarochan" target="_blank">Twitter</a></p>
         <p>
           (This is a sample website - you'll be building a site like this on {' '})
         </p>
